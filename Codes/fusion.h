cc2643c892fbbaa6ed7bbd0a2a6bcdd741e49a6c
sysu-xin-lab <59306847+sysu-xin-lab@users.noreply.github.com>
2020-03-18 16:52:44 +0800

    Update fusion.h
    fix compile errors in linux

--- Codes/fusion.h
+++ Codes/fusion.h
@@ -1,6 +1,14 @@
 #include<iostream>
 #include"PARAMETER.h"
 #include"cuLayer.h"
+
+#ifdef __unix
+	#define fopen_s(pFile,filename,mode) ((*(pFile))=fopen((filename),  (mode)))==NULL
+	#define sscanf_s(pBuffer,fmt,dst,num) sscanf((pBuffer),(fmt),(dst))
+	#define fscanf_s(pFile,fmt,pBuffer,num) fscanf((pFile),(fmt),(pBuffer))
+	#define strtok_s strtok_r
+#endif
+
 void Re_fusion(const char * BufferIn0,const char * BufferIn1,const char * BufferIn2,const char * BufferOut,int win_size,int classnum,float L_err,float M_err,int A,int pf,int pc);
 void usage(char *command);
 void runtest1(float *BufferLandsat_0,float * BufferModis_0,float *BufferModis_1,float *out,int height,int width,int win_size ,float L_err,float M_err,int classnum,int A);
